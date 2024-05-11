ab5690a10bdb9c5c7a608261680167fbe70dd462
mojiez <937691466@qq.com>
2024-05-11 18:47:38 +0800

    first commit 引入了Spring Cloud Gateway 解决了版本冲突问题 2.7.5的SpringBoot对应2021.0.3的SpringCloud

---
+++ src/main/java/com/atyichen/chenapigateway/ChenApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.atyichen.chenapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChenApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChenApiGatewayApplication.class, args);
+    }
+
+}
