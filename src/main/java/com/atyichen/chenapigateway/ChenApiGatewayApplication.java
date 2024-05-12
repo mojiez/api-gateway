3cd19865391c7cb0eb0049123f8ff5cb7f1acf6f
mojiez <937691466@qq.com>
2024-05-12 21:15:40 +0800

    测试了路由转发、断言

--- src/main/java/com/atyichen/chenapigateway/ChenApiGatewayApplication.java
+++ src/main/java/com/atyichen/chenapigateway/ChenApiGatewayApplication.java
@@ -3,6 +3,10 @@ package com.atyichen.chenapigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class ChenApiGatewayApplication {
 
@@ -10,4 +14,13 @@ public class ChenApiGatewayApplication {
         SpringApplication.run(ChenApiGatewayApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("to_baidu", r -> r.path("/baidu")
+                        .uri("http://www.baidu.com"))
+                .route("host_route", r -> r.host("*.myhost.org")
+                        .uri("http://httpbin.org"))
+                .build();
+    }
 }
